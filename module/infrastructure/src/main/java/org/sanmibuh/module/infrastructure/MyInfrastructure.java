59171b16de41a66ea82cfbda802459f5c9db33bc
David Belmez Vicente <sanmibuh@gmail.com>
2022-10-17 17:46:55 +0200

    [main]: infra without test fixtures

--- module/infrastructure/src/main/java/org/sanmibuh/module/infrastructure/MyInfrastructure.java
+++ module/infrastructure/src/main/java/org/sanmibuh/module/infrastructure/MyInfrastructure.java
@@ -1,10 +1,17 @@
 package org.sanmibuh.module.infrastructure;
 
+import org.sanmibuh.module.domain.MyDomain;
+import org.sanmibuh.module.projection.MyProjection;
+
 public class MyInfrastructure {
 
+  private final MyDomain myDomain;
+  private final MyProjection myProjection;
   private final String name;
 
-  public MyInfrastructure(final String name) {
+  public MyInfrastructure(final MyDomain myDomain, final MyProjection myProjection, final String name) {
+    this.myDomain = myDomain;
+    this.myProjection = myProjection;
     this.name = name;
   }
 
@@ -12,4 +19,11 @@ public class MyInfrastructure {
     return name;
   }
 
+  public MyDomain getMyDomain() {
+    return myDomain;
+  }
+
+  public MyProjection getMyProjection() {
+    return myProjection;
+  }
 }
--- module/infrastructure/src/test/java/org/sanmibuh/module/infrastructure/MyInfrastructureTest.java
+++ module/infrastructure/src/test/java/org/sanmibuh/module/infrastructure/MyInfrastructureTest.java
@@ -3,16 +3,25 @@ package org.sanmibuh.module.infrastructure;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.sanmibuh.module.domain.MyDomain;
+import org.sanmibuh.module.projection.MyProjection;
 
 class MyInfrastructureTest {
 
   @Test
   void should_infrastructure_run() {
-    final var name = "ANY";
+    final var name = "ANY Infra";
+    final var projectionName = "ANY projection";
+    final var domainName = "ANY domain";
 
-    final var myInfrastructure = new MyInfrastructure(name);
+    final var myProjection = new MyProjection(projectionName);
+    final var myDomain = new MyDomain(myProjection, domainName);
+
+    final var myInfrastructure = new MyInfrastructure(myDomain, myProjection, name);
 
     assertEquals(name, myInfrastructure.getName());
+    assertEquals(domainName, myInfrastructure.getMyDomain().getName());
+    assertEquals(projectionName, myInfrastructure.getMyProjection().getName());
   }
 
 }
